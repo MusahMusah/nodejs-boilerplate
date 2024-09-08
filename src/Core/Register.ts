43ab6f2c4566423d9b9d6d9d1af8bbeaa4f5cb52
MusahMusah <aliyumusahmusah@gmail.com>
2024-09-08 13:22:29 +0100

    feat: added sample module

--- src/Core/Register.ts
+++ src/Core/Register.ts
@@ -1,6 +1,9 @@
 import { Application } from "express";
 import { throttle } from "./middlewares/throttle.middleware";
+import { PREFIX } from "./config/constants";
+import sampleRoute from "../Modules/Sample/routes/sample.route"
 
 export const Register = (app: Application) => {
     app.use([throttle()]);
+    app.use(PREFIX + '/sample', sampleRoute);
 }
